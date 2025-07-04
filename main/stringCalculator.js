4ba33e85a306fa138051b42fc29bdb229037819e
Dharmik090 <dharmikparmarpd@gmail.com>
2025-07-05 03:44:23 +0530

    Test Passed: Input string contains delimiter of any length and numbers in separate lines

--- main/stringCalculator.js
+++ main/stringCalculator.js
@@ -3,11 +3,20 @@ function add(numbers = '') {
     let sum = 0;
     const delimiters = [',', '\n'];
 
-    if(numbers.startsWith('//')){
-        delimiters.push(numbers.substring(2,3));
-        numbers = numbers.substring(4);
+    if (numbers.startsWith('//')) {
+        if (numbers[2] === '[') {
+            const start = numbers.indexOf('[') + 1;
+            const end = numbers.indexOf(']');
+            const customDelimiter = numbers.substring(start, end);
+            
+            delimiters.push(customDelimiter);
+            numbers = numbers.substring(end + 2);
+        } else {
+            delimiters.push(numbers.substring(2, 3));
+            numbers = numbers.substring(4);
+        }
     }
-    
+
     const delimiterRegex = new RegExp(`[${delimiters.join('')}]`);
 
     numbers = numbers.split(delimiterRegex);
