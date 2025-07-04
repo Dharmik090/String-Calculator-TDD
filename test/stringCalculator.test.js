d13933eee94ce0f384266b0ce38c676f75a74c3d
Dharmik090 <dharmikparmarpd@gmail.com>
2025-07-05 03:55:10 +0530

    Test Failed: Input string contains negative numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -50,5 +50,11 @@ describe('Test String Calculator Method', () => {
         expect(add('//[:)]\n1:)2:)3')).toBe(6);
 
     });
+    
+    test('Input string contains negative numbers', () => {
+
+        expect(() => add('//;\n-1;-2')).toThrow(new Error('negative numbers not allowed -1, -2'));
+
+    });
 
 });
