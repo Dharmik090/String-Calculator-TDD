5cbc3dbf8d98d3ed4e47500cb57e823b266b6261
Dharmik090 <dharmikparmarpd@gmail.com>
2025-07-05 02:45:45 +0530

    Test Passed: Input string contains new line delimiter between numbers

--- main/stringCalculator.js
+++ main/stringCalculator.js
@@ -1,13 +1,18 @@
 function add(numbers = '') {
 
     let sum = 0;
-    numbers = numbers.split(',');
+    const delimiters = [',', '\n'];
+
+    const delimiterRegex = new RegExp(`[${delimiters.join('')}]`);
+
+    numbers = numbers.split(delimiterRegex);
 
     for (let number of numbers) {
-        sum = sum + parseInt(number || '0', 10);
+        sum += parseInt(number || '0', 10);
     }
 
     return sum;
+
 }
 
 module.exports = add;
