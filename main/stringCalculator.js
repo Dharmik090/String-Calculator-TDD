2dc12385c4b8152351599c6a323d5cd258997dad
Dharmik090 <dharmikparmarpd@gmail.com>
2025-07-05 03:58:13 +0530

    Test Passed: Input string contains negative numbers

--- main/stringCalculator.js
+++ main/stringCalculator.js
@@ -2,13 +2,14 @@ function add(numbers = '') {
 
     let sum = 0;
     const delimiters = [',', '\n'];
+    const negativeNumbers = [];
 
     if (numbers.startsWith('//')) {
         if (numbers[2] === '[') {
             const start = numbers.indexOf('[') + 1;
             const end = numbers.indexOf(']');
             const customDelimiter = numbers.substring(start, end);
-            
+
             delimiters.push(customDelimiter);
             numbers = numbers.substring(end + 2);
         } else {
@@ -22,7 +23,17 @@ function add(numbers = '') {
     numbers = numbers.split(delimiterRegex);
 
     for (let number of numbers) {
-        sum += parseInt(number || '0', 10);
+        let value = parseInt(number || '0', 10);
+
+        if (value < 0) {
+            negativeNumbers.push(value);
+        }
+
+        sum += value;
+    }
+
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(', ')}`);
     }
 
     return sum;
