d03480d06691b6ae5f48ac30cfa1c680b07a7219
Dharmik090 <dharmikparmarpd@gmail.com>
2025-07-05 02:00:49 +0530

    Refactored: Input string with zero, one or two numbers returns correct sum

--- main/stringCalculator.js
+++ main/stringCalculator.js
@@ -1,13 +1,8 @@
-function add( numbers ) {
+function add( numbers = '' ) {
 
-    if(numbers === '')
-        return 0;
-
-    if(numbers === '1')
-        return 1;
-
-    if(numbers == '1,5')
-        return 6;
+    const [number1, number2] = numbers.split(',');
+    
+    return parseInt(number1 || '0', 10) + parseInt(number2 || '0', 10);
     
 }
 
